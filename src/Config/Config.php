4d8b5b95389e52bf76e85551293e40350506eb7a
ahmedmt3 <85899361+ahmedmt3@users.noreply.github.com>
2024-08-14 15:48:45 +0300

    Update Config.php

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -5,19 +5,14 @@ namespace App\Config;
 class Config
 {
 
-    // Database Host name
-    private const DB_HOST = "localhost";
-    // Database Name
-    private const DB_NAME = "notes_app";
-    // Database username
-    private const DB_USER = "root";
-    // Database Password
-    private const DB_PASS = "";
+    public const DB_CONFIG = [
 
-    public static const DB_CONFIG = [
-        'DB_HOST' => $this::DB_HOST,
-        'DB_NAME' => $this::DB_NAME,
-        'DB_USER' => $this::DB_USER,
-        'DB_NAME' => $this::DB_PASS
+        'DB_HOST' => "localhost", // Database Host name
+
+        'DB_NAME' => "notes_app", // Database Name
+
+        'DB_USER' => "root", // Database Username
+
+        'DB_PASS' => "" // Database Password
     ];
 }
